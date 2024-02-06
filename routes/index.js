54703e5ae1252fd7041316f55647440b46bcfd77
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-02-06 15:24:32 -0500

    successfully display array messages on template view index.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+
+  const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+  res.render('index', {title: 'Express', messages: messages});
 });
 
-router.get('/new', function(req, res, next){
-  res.render('new-msg', {title: 'New Message'})
+router.get('/new', function (req, res, next) {
+  res.render('new-msg', { title: 'New Message' })
 })
 module.exports = router;
